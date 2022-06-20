cec63954d8bc79727fa5cf4eda31d21a4e8230b3
Fonseca <matheusfonsecade@hotmail.com>
2022-06-20 19:14:07 -0300

    requisitos 3 e 4 concluidos

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../pages';
+import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 4', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+  it('Verifica se contém um heading h2 com o texto Page requested not found 😭', () => {
+    const page = screen.getByRole('heading',
+      { name: /Page requested not found/i, level: 2 });
+    expect(page).toBeInTheDocument();
+  });
+  it('Verifica se a página mostra a imagem', () => {
+    const havImg = screen.getByRole('img',
+      { src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif' });
+    expect(havImg).toHaveAttribute('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(havImg).toBeInTheDocument();
+  });
+});
